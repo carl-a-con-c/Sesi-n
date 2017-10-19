4b5198e4ebcef1c248d037d71148896d4ff1df7f
carl-a-con-c <30882351+carl-a-con-c@users.noreply.github.com>
2017-10-19 08:57:00 -0500

    Add files via upload

---
+++ EjemploSession-master/EjemploSession/src/java/servlets/ValidaSesionesServlet.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class ValidaSesionesServlet extends HttpServlet {
+   
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+      response.setContentType("text/html");
+      HttpSession sesion = request.getSession();
+      String titulo = null;
+
+      //Pedimos el atributo, y verificamos si existe
+      String claveSesion = (String) sesion.getAttribute("claveSesion");
+
+      if(claveSesion.equals("emmanueloropeza")){
+        try(PrintWriter out=response.getWriter())
+        {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Good</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<center><h1>Bienvenido</h1></center>");
+            out.println("<center><h1>Carla Gomez</h1></center>");
+            out.println("<img src='cerdito.jpg'>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+      }
+      else
+      {
+          try(PrintWriter out=response.getWriter())
+        {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Error</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<center><h1>Error</h1></center>");
+            out.println("<img src='tache.png'>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+      }
+
+
+      //Mostramos los  valores en el cliente
+      PrintWriter out = response.getWriter();
+      out.println("¿Continua la Sesion y es valida?: " + titulo);
+      out.println("<br>");
+      out.println("ID de la sesi&oacute;n JSESSIONID: " + sesion.getId());
+  
+    }
+
+}
